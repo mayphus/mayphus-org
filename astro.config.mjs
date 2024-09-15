98c925fa1d5eb896e87131c4b7fd3b7c0218ae7c
felixmurraytang <tangmeifa@gmail.com>
2024-09-15 21:35:19 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import org from 'astro-org';
-import { customKeywordsToFrontmatter } from './src/lib/keywords';
+import { config } from './src/lib/config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://felixmurraytang.com',
 	integrations: [
-		org({
-			uniorgPlugins: [customKeywordsToFrontmatter],
-		}),
+		org(config),
 		sitemap(),
 	],
 });
