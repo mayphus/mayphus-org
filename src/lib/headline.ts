48cd504d9a55af5175e9bcd28f8b45c71faaa6d3
felixmurraytang <tangmeifa@gmail.com>
2024-09-20 22:28:07 +0800

    fix

--- src/lib/headline.ts
+++ src/lib/headline.ts
@@ -3,15 +3,9 @@ import { visit } from 'unist-util-visit';
 interface Headline extends Node {
     type: 'headline'
     level: number
-    data: {
-        hProperties: {
-            style: string
-        }
-        id: string
-    }
 }
 
-export function customHeadline() {
+export function customHeadline() {  
   return (tree: any) => {
     visit(tree, 'headline', (node: Headline) => { 
       if (node.level < 6) {
