767f8e8c91a8123db1bd10a512a5cd7e46cfe89f
felixmurraytang <tangmeifa@gmail.com>
2024-09-23 20:56:34 +0800

    modify keywords.astro to keyword.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 import org from './src/lib/astro-org';
-import { customKeywords } from './src/lib/plugins/keywords';
+import { customKeywords } from './src/lib/plugins/keyword';
 import { customHeadline } from './src/lib/plugins/headline';
 
 // https://astro.build/config
