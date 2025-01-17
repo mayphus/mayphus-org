bac0a4b0bc70e8e39789d48af0486d14a3e668f8
Mayphus T'ang <tangmeifa@gmail.com>
2025-01-17 22:44:04 +0800

    feat: update site title and description; revise skills list in index.astro

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Mayphus Tang';
-export const SITE_DESCRIPTION = 'mayphus tang\'s personal website';
+export const SITE_TITLE = "Mayphus T'ang";
+export const SITE_DESCRIPTION = "A dad, a maker, a perpetual learner";
